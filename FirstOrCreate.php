db2b0e163cd7455af6098904596623a9737b143d
Oleg Pustovalov <oleg@portalnews.ru>
2018-06-10 00:18:48 +0300

    init

---
+++ FirstOrCreate.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Методы для поиска записи (модели) или создания если не найдено
+ */
+
+namespace common\traits\models;
+
+
+//use yii\db\ActiveRecord;
+
+trait FirstOrCreate
+{
+    /**
+     * Возвращает модель по переданным атрибутам,
+     * если модель не найдена, то создаём и возвращаем новую модель
+     *
+     * @param array $attributes
+     * @return ActiveRecord FirstOrCreate
+     */
+    public static function firstOrNew($attributes)
+    {
+        $model = self::findOne($attributes);
+
+        if($model === null){
+            $model = new self($attributes);
+        }
+
+        return $model;
+    }
+
+    /**
+     * Возвращает модель по переданным атрибутам,
+     * если модель не найдена, то создаём, сохраняем и возвращаем новую модель
+     *
+     * @param array $attributes
+     * @param bool $runValidation
+     * @return ActiveRecord
+     */
+    public static function firstOrCreate($attributes, $runValidation = false)
+    {
+        $model = self::firstOrNew($attributes);
+
+        if($model->isNewRecord){
+            $model->save($runValidation);
+        }
+
+        return $model;
+    }
+}
